e375ce823f647d4b9d6f2143dcdde3dc6e7a498e
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-09-04 16:21:57 +0530

    Completed tests and contract

--- contracts/NFTbeats.sol
+++ contracts/NFTbeats.sol
@@ -13,9 +13,11 @@ contract NFTbeats is ERC721 {
     }
 
     uint public tCount;
+    uint public oCount;
     
     mapping(address => string) artists;
     mapping(uint => Track) public tracks;
+    mapping(uint => Offer) public offers;
 
     struct Track {
         uint id;
@@ -27,9 +29,56 @@ contract NFTbeats is ERC721 {
         address payable owner;
     }
 
-    event createdTrack(uint id, string name, address owner);
-    event setTrackPrice(uint id, uint newPrice, bool listed);
-    event boughtTrack(uint id, address buyer);
+    struct Offer {
+        uint id;
+        uint trackID;
+        uint offerAmount;
+        address bidder;
+        address auctioneer;
+        bool isApproved;
+    }
+
+    event createdTrack(
+        uint id, 
+        string name, 
+        address owner
+    );
+
+    event setTrackPrice(
+        uint id, 
+        uint newPrice, 
+        bool listed
+    );
+
+    event offerApproved(
+        uint id,
+        uint trackID, 
+        uint amount, 
+        bool approved
+    );
+
+    event boughtTrack(
+        uint id, 
+        address buyer
+    );
+
+    function registerArtist(string memory _name) external {
+        artists[msg.sender] = _name;
+    }
+
+    function verifyCreator(uint _id) 
+        external 
+        view 
+        idExists(_id)
+        returns (string memory) 
+    {
+        address creator = tracks[_id].artist;
+        if(bytes(artists[creator]).length > 0) {
+            return artists[creator];
+        } else {
+            return "Address not recognized";
+        }
+    }
 
     function createTrack(
         string memory _name, 
@@ -51,9 +100,27 @@ contract NFTbeats is ERC721 {
     {
         tracks[_id].isListed = true;
         tracks[_id].price = _price;
-        approve(address(this), _id);
         emit setTrackPrice(_id, tracks[_id].price, tracks[_id].isListed);
     } 
+
+    function makeOffer(
+        uint _tid, 
+        uint _amount
+    ) 
+        external 
+        payable 
+        idExists(_tid)
+    {
+        require(_amount > tracks[_tid].price, "Invalid offer");
+        oCount++;    
+        offers[oCount] = Offer(oCount, _tid, _amount, msg.sender, tracks[_tid].owner, false);
+    }
+
+    function approveOffer(uint _id) external {
+        approve(offers[_id].bidder, offers[_id].trackID);
+        offers[_id].isApproved = true;
+        emit offerApproved(oCount, offers[_id].trackID, offers[_id].offerAmount, offers[_id].isApproved);
+    }
   
     function buyTrack(uint _id)
         external
@@ -62,9 +129,9 @@ contract NFTbeats is ERC721 {
     {
         (bool paid,) = tracks[_id].owner.call{ value: msg.value }("");
         require(paid == true, "Not sent");
-        _transfer(tracks[_id].owner, msg.sender, _id);
+        safeTransferFrom(tracks[_id].owner, msg.sender, _id);
         tracks[_id].owner = msg.sender;
         tracks[_id].isListed = false;
-        emit boughtTrack(_id, msg.sender);
+        emit boughtTrack(_id, tracks[_id].owner);
     }
 }
--- test/nftbeatstest.js
+++ test/nftbeatstest.js
@@ -48,13 +48,13 @@ contract("Nfto", async (accounts) => {
         })
     })
 
-    describe("Listing track for sale", async () => {
+    describe("Listing track for sale/auction", async () => {
         let event;
 
         before(async () => {
             await nftb.createTrack("Test", "fweig23385t3dfa", { from: currentAcc });
             const result = await nftb.setPrice(1, web3.utils.toWei("0.5", "Ether"), { from: currentAcc });
-            event = result.logs[1].args;
+            event = result.logs[0].args;
         })
 
         it("can set new price", async () => {
@@ -64,25 +64,65 @@ contract("Nfto", async (accounts) => {
         it("listed for sale", async () => {
             assert.equal(event.listed, true);
         })
+    })
 
-        it("approves contract for transfer", async () => {
+    describe("Approve offers", async () => {
+        let buyer, event;
+
+        before(async () => {
+            buyer = accounts[1];
+            await nftb.makeOffer(1, web3.utils.toWei("0.6", "Ether"), { from: buyer });
+            const result = await nftb.approveOffer(1, { from: currentAcc });
+            event = result.logs[1].args;
+        })
+
+        it("approves transfer to bidder", async () => {
             const approvedAddr = await nftb.getApproved(1);
-            assert.equal(approvedAddr, nftb.address);
+            assert.equal(approvedAddr, buyer);
+        })
+
+        it("approves offer", async () => {
+            assert.equal(event.id, 1);
+            assert.equal(event.trackID, 1);
+            assert.equal(web3.utils.fromWei(event.amount.toString(), "Ether"), 0.6);
+            assert.equal(event.approved, true);
         })
     })
 
     describe("Purchase NFT tracks", async () => {
-        let b1, buyer;
+        let b1, buyer, event;
 
         before(async () => {
             b1 = await web3.eth.getBalance(currentAcc);
             buyer = accounts[1];
-            await nftb.buyTrack(1, { from: buyer, value: web3.utils.toWei("0.5", "Ether") });
+            const result = await nftb.buyTrack(1, { from: buyer, value: web3.utils.toWei("0.6", "Ether") });
+            event = result.logs[2].args;
         })
 
         it("pays the owner", async () => {
             const b2 = await web3.eth.getBalance(currentAcc);
-            assert.equal(web3.utils.fromWei(b2.toString()) - web3.utils.fromWei(b2.toString(), 0.5));
+            assert.equal((web3.utils.fromWei(b2.toString()) - web3.utils.fromWei(b1.toString())) >= 0.59, true);
+        })
+
+        it("transfers the NFT", async () => {
+            assert.equal(event.id.toNumber(), 1);
+            assert.equal(event.buyer, buyer);
+            const newOwner = await nftb.ownerOf(1);
+            assert.equal(newOwner, buyer);
+        })
+    })
+
+    describe("Verify track creator", async () => {
+        let artist;
+
+        before(async () => {
+            artist = accounts[0];
+            await nftb.registerArtist("JZX", { from: artist });
+        })
+
+        it("verifies correct creator", async () => {
+            const creator = await nftb.verifyCreator(1);
+            assert.equal(creator, "JZX");
         })
     })
 })
