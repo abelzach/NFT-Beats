6276f862e0b2a998eeb9f003dcd545295b33199e
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2021-09-04 11:35:17 +0530

    Modified contract and created tests

--- contracts/NFTbeats.sol
+++ contracts/NFTbeats.sol
@@ -5,20 +5,34 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTbeats is ERC721 {
   
-  constructor() ERC721("NFTbeats", "BEATS") {}
+    constructor() ERC721("NFTbeats", "BEATS") {}
 
-  uint public tCount;
-  
-  mapping(address => string) artists;
-  mapping(uint => Track) public tracks;
+    uint public tCount;
+    
+    mapping(address => string) artists;
+    mapping(uint => Track) public tracks;
 
-  struct Track {
-    uint id;
-    uint price;
-    bool isListed;
-    string name;
-    string filecid;
-    address artist;
-    address payable owner;
-  }
+    struct Track {
+      uint id;
+      uint price;
+      bool isListed;
+      string name;
+      string filecid;
+      address artist;
+      address payable owner;
+    }
+
+    event createdTrack(uint, string, address);
+
+    function createTrack(
+        string memory _name, 
+        string memory _filecid
+    )   external {
+        require(bytes(_name).length > 0 && bytes(_filecid).length > 0, "Name and file required");
+        tCount++;
+        _safeMint(msg.sender, tCount);
+        _setTokenURI(tCount, _filecid);
+        tracks[tCount] = Track(tCount, 0, false, _name, _filecid, msg.sender, msg.sender);
+        emit createdTrack(tCount, tracks[tCount].name, tracks[tCount].owner);
+    }    
 }
---
+++ tests/nftbeatstest.js
@@ -0,0 +1,5 @@
+const NFTbeats = artifacts.require("NFTbeats")
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .should()
